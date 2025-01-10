e87c1188ed1dde249979fff9cd2331ed3defa782
Atul Kamath <realatulkamath@gmail.com>
2025-01-10 23:55:13 +0400

    Added top header.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { MenuIcon, ShoppingCart, User } from "lucide-react";
+import AmazonLogo from "./AmazonLogo";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="flex bg-amazon-blue items-center px-2">
+      <MenuIcon color="white" />
+      <AmazonLogo className="white" />
+      <div className="flex ml-auto items-center px-4 space-x-2">
+        <button className="flex items-center text-xs">
+          Sign in &gt; <User color="white" />
+        </button>
+        <ShoppingCart color="white" />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "amazon-blue": "#232F3E",
       },
     },
   },
