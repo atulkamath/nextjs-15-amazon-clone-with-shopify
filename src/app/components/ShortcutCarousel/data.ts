b3cb2d2f3f373a0b73dd292a42492c788337fe82
Atul Kamath <realatulkamath@gmail.com>
2025-02-14 16:08:03 +0400

    added carousels to hero.

---
+++ src/app/components/ShortcutCarousel/data.ts
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    title: "Outlet",
+    image: "https://m.media-amazon.com/images/I/310F6fk-mnL._SX100_SY100_.jpg",
+    link: "/save-more",
+  },
+  {
+    title: "Deals",
+    image: "https://m.media-amazon.com/images/I/21kCA-5TaCL._SX100_SY100_.png",
+    link: "/deals",
+  },
+  {
+    title: "Mobiles",
+    image: "https://m.media-amazon.com/images/I/118ZsmgnR7L._SX100_SY100_.jpg",
+    link: "/mobiles",
+  },
+  {
+    title: "Fashion",
+    image: "https://m.media-amazon.com/images/I/213FPT-OXwL._SX100_SY100_.png",
+    link: "/fashion",
+  },
+  {
+    title: "Audio",
+    image: "https://m.media-amazon.com/images/I/21YVpRG06BL._SX100_SY100_.jpg",
+    link: "/audio",
+  },
+  {
+    title: "TVs",
+    image: "https://m.media-amazon.com/images/I/31wyl9i6GtL._SX100_SY100_.jpg",
+    link: "/tv",
+  },
+  {
+    title: "Books",
+    image: "https://m.media-amazon.com/images/I/21ypyWWMXYL._SX100_SY100_.png",
+    link: "/books",
+  },
+];
